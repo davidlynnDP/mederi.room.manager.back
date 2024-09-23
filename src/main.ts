0d469862dd8c59f951aa0b8f24b247bbef38ef6a
David Leon <davidleon21goal@gmail.com>
2024-09-23 10:56:41 -0500

    last commit

--- src/main.ts
+++ src/main.ts
@@ -26,10 +26,17 @@ async function bootstrap() {
   logger.log(`App running on port ${ envs.port }`);
 }
 bootstrap();
+//guia para echar a andar la aplicacion
+//cp .env.template .env
+//npm install
 //docker-compose up -d
 //npm run start:dev
+//npx prisma db push
 
-//npm i prisma --save-dev
-//npx prisma init
-//npm i @prisma/client
-//npx prisma migrate dev --name init | npx prisma db push
+//guia de como se puede desplegar la aplicacion
+//docker-compose up -d
+//docker-compose -f docker-compose.prod.yml build
+//docker-compose -f docker-compose.prod.yml up
+//docker push dav1dlynn/mederi.room.manager.back:latest
+
+//docker buildx build --platform linux/amd64,linux/arm64 -t dav1dlynn/mederi.room.manager.back --push .
